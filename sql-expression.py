1b234595e202975b172a93e13b6b39235e64b912
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-02-21 19:12:40 +0000

    Sql

--- sql-expression.py
+++ sql-expression.py
@@ -11,37 +11,40 @@ meta = MetaData(db)
 # create variable for the "Artist" table
 artist_table = Table(
     "artist", meta,
-    Column("artist_id", integer, primary_key=True),
+    Column("artist_id", Integer, primary_key=True),
     Column("name", String)
 )
 
 # create variable for "Album" table
 album_table = Table(
     "album", meta,
-    Column("album_id", integer, primary_key=True),
+    Column("album_id", Integer, primary_key=True),
     Column("title", String),
-    Column("artist_id", integer, ForeignKey("artist_table.artist_id")),
-    Column("album_id", integer, primary_key=False),
+    Column("artist_id", Integer, ForeignKey("artist_table.artist_id"))
 )
 
 # create variable for "Track" table
 track_table = Table(
     "track", meta,
-    Column("track_id", integer, primary_key=True),
+    Column("track_id", Integer, primary_key=True),
     Column("name", String),
-    Column("media_type_id", integer, ForeignKey("album_table.album_id")),
-    Column("genre_id", integer, primary_key=False),
-    Column("Composer", String),
-    Column("Milliseconds", Integer),
-    Column("Bytes", Integer),
-    Column("UnitPrice", Float)
+    Column("album_id", Integer, ForeignKey("album_table.album_id")),
+    Column("media_type_id", Integer, primary_key=False),
+    Column("genre_id", Integer, primary_key=False),
+    Column("composer", String),
+    Column("milliseconds", Integer),
+    Column("bytes", Integer),
+    Column("unit_price", Float)
 )
 
 # Making the connection
 with db.connect() as connection:
 
     # Query 1 - select all records from the "Artist" table
-    select_query = artist_table.select()
+    # select_query = artist_table.select()
+
+    # Query 2 - select only the "Name" column from the "Artist" table
+    select_query = artist_table.select().with_only_columns([])
 
     results = connection.execute(select_query)
     for result in results:
